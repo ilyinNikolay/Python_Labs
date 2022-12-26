114e7642b7e552c5a06e8e6f4a45f533660341d6
Михаил Мальцев <70252426+maltsevmihail-ris-21-1b@users.noreply.github.com>
2022-12-26 19:38:33 +0500

    Add files via upload

---
+++ lab3/main.py
@@ -0,0 +1,19 @@
+N = 8888
+
+from random import randint
+from pyDatalog import pyDatalog
+pyDatalog.create_terms('X, Sum, Avg, RandSum, RandMed, Res, Summary, Range, Length, ListSummary, Mas, Median, RandMas')
+
+(Summary[Range] == sum_(X, for_each = X)) <= X.in_(range_(Range + 1))
+print(f"{Sum == Summary[N]}\n")
+
+(Length[Range] == len_(X, for_each = X)) <= X.in_(range_(Range + 1))
+print(f"{Avg == (Summary[N] / Length[N])}\n")
+
+RandMas = [randint(0, 99) for t in range(0, 100)]
+RandMas.sort()
+(ListSummary[Mas] == sum_(X, for_each = X)) <= X.in_(Mas)
+print(f"{RandSum == ListSummary[RandMas]}\n")
+
+(Res == Median[Mas]) <= (Res == (Mas[49] + Mas[50]) / 2)
+print(f"{RandMed == Median[RandMas]}\n")
