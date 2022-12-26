6df576b4871fcf5101d070fc63a4655a05ac86ee
Михаил Мальцев <70252426+maltsevmihail-ris-21-1b@users.noreply.github.com>
2022-12-26 19:45:00 +0500

    Update main.py

--- lab3/main.py
+++ lab3/main.py
@@ -5,15 +5,15 @@ from pyDatalog import pyDatalog
 pyDatalog.create_terms('X, Sum, Avg, RandSum, RandMed, Res, Summary, Range, Length, ListSummary, Mas, Median, RandMas')
 
 (Summary[Range] == sum_(X, for_each = X)) <= X.in_(range_(Range + 1))
-print(f"{Sum == Summary[N]}\n")
+print(f"{Sum == Summary[N]}")
 
 (Length[Range] == len_(X, for_each = X)) <= X.in_(range_(Range + 1))
-print(f"{Avg == (Summary[N] / Length[N])}\n")
+print(f"{Avg == (Summary[N] / Length[N])}")
 
 RandMas = [randint(0, 99) for t in range(0, 100)]
 RandMas.sort()
 (ListSummary[Mas] == sum_(X, for_each = X)) <= X.in_(Mas)
-print(f"{RandSum == ListSummary[RandMas]}\n")
+print(f"{RandSum == ListSummary[RandMas]}")
 
 (Res == Median[Mas]) <= (Res == (Mas[49] + Mas[50]) / 2)
-print(f"{RandMed == Median[RandMas]}\n")
+print(f"{RandMed == Median[RandMas]}")
